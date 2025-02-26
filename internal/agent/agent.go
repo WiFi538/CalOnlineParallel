7b1658e560ffca51383b2bc6784a068c79bb8496
Филиппов Иван Сергеевич <ivsefilippov@edu.hse.ru>
2025-02-26 20:23:24 +0300

    add agent.go

--- internal/agent/agent.go
+++ internal/agent/agent.go
@@ -1,21 +1,19 @@
-package main
+package agent
 
 import (
 	"log"
 	"os"
 	"strconv"
-
-	"github.com/wifi538/CalOnlineParallel/internal/agent"
 )
 
-func main() {
+func InitAgent() {
 	computingPower, err := strconv.Atoi(os.Getenv("COMPUTING_POWER"))
 	if err != nil {
 		log.Fatalf("Invalid COMPUTING_POWER: %v", err)
 	}
 
 	for i := 0; i < computingPower; i++ {
-		go agent.Worker()
+		go Worker()
 	}
 
 	select {}
