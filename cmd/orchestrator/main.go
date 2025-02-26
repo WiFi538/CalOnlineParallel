21a9e179209ddc4d5776229c45b43f39e9269fcf
Филиппов Иван Сергеевич <ivsefilippov@edu.hse.ru>
2025-02-26 20:14:55 +0300

    change orchestrator/main.go

--- cmd/orchestrator/main.go
+++ cmd/orchestrator/main.go
@@ -4,16 +4,19 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/wifi538/CalOnlineParallel/internal/orchestrator"
 )
 
 func main() {
-	http.HandleFunc("/api/v1/calculate", orchestrator.HandleCalculate)
-	http.HandleFunc("/api/v1/expressions", orchestrator.HandleGetExpressions)
-	http.HandleFunc("/api/v1/expressions/:id", orchestrator.HandleGetExpressionByID)
-	http.HandleFunc("/internal/task", orchestrator.HandleGetTask)
-	http.HandleFunc("/internal/task", orchestrator.HandlePostTaskResult)
+	r := mux.NewRouter()
+
+	r.HandleFunc("/api/v1/calculate", orchestrator.HandleCalculate).Methods("POST")
+	r.HandleFunc("/api/v1/expressions", orchestrator.HandleGetExpressions).Methods("GET")
+	r.HandleFunc("/api/v1/expressions/{id}", orchestrator.HandleGetExpressionByID).Methods("GET")
+	r.HandleFunc("/internal/task", orchestrator.HandleGetTask).Methods("GET")
+	r.HandleFunc("/internal/task", orchestrator.HandlePostTaskResult).Methods("POST")
 
 	log.Println("Orchestrator is running on :8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
