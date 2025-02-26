37ba6a5d90ad30166df54de490b7fff56a74ec33
Филиппов Иван Сергеевич <ivsefilippov@edu.hse.ru>
2025-02-26 20:06:12 +0300

    add task

---
+++ internal/orchestrator/task.go
@@ -0,0 +1,9 @@
+package orchestrator
+
+type Task struct {
+	ID            string `json:"id"`
+	Arg1          string `json:"arg1"`
+	Arg2          string `json:"arg2"`
+	Operation     string `json:"operation"`
+	OperationTime int    `json:"operation_time"`
+}
